d1157f525cc45007373c1b898e24ea285a22ff09
okiromosh <moses.okiror93@gmail.com>
2025-09-04 16:36:35 +0300

    m

--- drinks/views.py
+++ drinks/views.py
@@ -2,13 +2,27 @@ from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
+
 def drink_list(request):
-    #get the drinks
-    drinks = Drink.objects.all()
-    # serialize them
-    serializer = DrinkSerializer(drinks, many=True)
-    # return the json to user
-    return JsonResponse({'drinks':serializer.data}, safe=False)
+
+    if request.method == 'GET':
+        #get the drinks
+        drinks = Drink.objects.all()
+        # serialize them
+        serializer = DrinkSerializer(drinks, many=True)
+        # return the json to user
+        return JsonResponse({'drinks':serializer.data}, safe=False)
+
+    if request.method == 'POST':
+        # get data from user input
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        
